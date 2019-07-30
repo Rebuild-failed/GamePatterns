6ce5500c03707a2825abe842f2db93b8b051e931
Rebuild(failed) <764668210@qq.com>
2019-07-30 23:04:02 +0800

    + Observer

---
+++ GamePatters/Assets/Observer/Scripts/EventFactor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EventType
+{
+    Refresh_Text
+}
+public delegate void EventDelegate();
+public class EventFactor : MonoBehaviour
+{
+    private static Dictionary<EventType, EventDelegate> eventCenter = new Dictionary<EventType, EventDelegate>();
+
+    public static void AddListener(EventType eventType, EventDelegate eventDelegate)
+    {
+        if(eventCenter.ContainsKey(eventType))
+        {
+            eventCenter[eventType] += eventDelegate;
+        }
+        else
+        {
+            eventCenter.Add(eventType, eventDelegate);
+        }
+        
+    }
+    public static void RemoveListener(EventType eventType, EventDelegate eventDelegate)
+    {
+        if (eventCenter.ContainsKey(eventType))
+        {
+            eventCenter[eventType] -= eventDelegate;
+        }
+    }
+    public static void Notify(EventType eventType)
+    {
+        EventDelegate eventDelegate;
+        if (eventCenter.TryGetValue(eventType, out eventDelegate))
+        {
+            eventDelegate();
+        }
+    }
+}
---
+++ GamePatters/Assets/Observer/Scripts/UIText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIText : MonoBehaviour
+{
+    public Text text;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        EventFactor.AddListener(EventType.Refresh_Text, RefreshText);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnDisable()
+    {
+        EventFactor.RemoveListener(EventType.Refresh_Text, RefreshText);
+    }
+    void RefreshText()
+    {
+        text.text = "任务完成";
+    }
+}
